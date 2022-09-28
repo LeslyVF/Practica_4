fde6a5691b168f5458885e306b046727781bc851
armentaspcs <armentaspcs@DESKTOP-AHUAJBN>
2022-09-27 18:12:27 -0700

    Agregar archivos de proyecto.

---
+++ Practica_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c, f;
+
+            Console.WriteLine("Dame los grados centigrados");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            f = (c * 1.8) +32;
+
+            Console.WriteLine("Los grados Farenheit son:{0}", f);
+        }
+    }
+}
